01dc7c990e449fc9b7d4d0923bb1bf468891432c
gindis.wx <gindis.wx@alibaba-inc.com>
2019-12-12 16:37:21 +0800

    feat: update tpl

--- assets/src/mods/mod16465/index.js
+++ assets/src/mods/mod16465/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import './index.css';
+'use strict';
 
-function Mod() {
-  return (
-    <div className={'mod16465 block'}>
-      <img src={'https://img.alicdn.com/tfs/TB1dZPqpxD1gK0jSZFKXXcJrVXa-236-236.png'} className={'pic'} />
-      <span className={'imgcook'}>imgcook</span>
-      <span className={'info'}>imgcook pull -a 「moduleId」</span>
-      <span className={'info_2'}>来拉取您的应用</span>
-      <span className={'desc'}>阿里经济体前端委员会 · 智能化小组出品</span>
-    </div>
-  );
+import React, { Component } from 'react';
+
+import styles from './style.js';
+
+class Page_0 extends Component {
+  render() {
+    const url = 'https://img.alicdn.com/tfs/TB1dZPqpxD1gK0jSZFKXXcJrVXa-236-236.png';
+    return (
+      <div style={styles.block}>
+        <img style={styles.pic} src={url} alt="" />
+        <span style={styles.imgcook}>imgcook</span>
+        <span style={styles.info}>imgcook pull -a 「moduleId」</span>
+        <span style={styles.info_2}>来拉取您的应用</span>
+        <span style={styles.desc}>阿里经济体前端委员会 · 智能化小组出品</span>
+      </div>
+    );
+  }
 }
-
-export default Mod;
+export default Page_0;
---
+++ assets/src/mods/mod16465/style.js
@@ -0,0 +1,60 @@
+export default {
+  block: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    borderRadius: '1.60vw',
+    backgroundColor: '#ffffff',
+    width: '93.60vw',
+    height: '55.20vw',
+    overflow: 'hidden'
+  },
+  pic: { marginTop: '6.00vw', width: '15.73vw', height: '15.73vw' },
+  imgcook: {
+    marginTop: '1.33vw',
+    maxWidth: '56.80vw',
+    height: '5.33vw',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    lineHeight: '5.33vw',
+    whiteSpace: 'nowrap',
+    color: '#333333',
+    fontSize: '4.80vw',
+    fontWeight: 600
+  },
+  info: {
+    marginTop: '5.07vw',
+    maxWidth: '65.87vw',
+    height: '3.20vw',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    lineHeight: '3.20vw',
+    whiteSpace: 'pre',
+    color: '#999999',
+    fontSize: '2.67vw',
+    fontWeight: 400
+  },
+  info_2: {
+    maxWidth: '56.67vw',
+    height: '3.20vw',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    lineHeight: '3.20vw',
+    whiteSpace: 'nowrap',
+    color: '#999999',
+    fontSize: '2.67vw',
+    fontWeight: 400
+  },
+  desc: {
+    marginTop: '8.27vw',
+    maxWidth: '71.07vw',
+    height: '3.20vw',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    lineHeight: '3.20vw',
+    whiteSpace: 'pre',
+    color: '#d4d4d4',
+    fontSize: '2.67vw',
+    fontWeight: 400
+  }
+};
