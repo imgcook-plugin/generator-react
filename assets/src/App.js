61bd39ae4f5915e372171056d904365fe48a12e0
波本 <tingpeng.ttp@alibaba-inc.com>
2019-12-19 14:53:06 +0800

    chore: remove useless

--- assets/src/App.js
+++ assets/src/App.js
@@ -9,8 +9,10 @@ function App() {
         <img src="https://gw.alicdn.com/tfs/TB1YzajyNnaK1RjSZFtXXbC2VXa-200-64.png" alt="imgcook" />
       </div>
       {
-        Object.keys(mods).length ? Object.keys(mods).map((modName) => {
-          return React.createElement(mods[modName]);
+        Object.keys(mods).length ? Object.keys(mods).map((modName, index) => {
+          return <div key={index}>
+            {React.createElement(mods[modName])}
+          </div>;
         }) : <span>{'请在项目根目录执行 imgcook pull <moduleId> --path ./src/mods/<moduleId>'}</span>
       }
     </div>
